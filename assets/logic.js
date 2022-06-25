feea3ac332b0cc7dc0f1aac01e527a98158a9912
Victor Shaviya <vs610642@digicelgroup.com>
2022-06-25 13:54:51 -0500

    Create new elements for holding the created todo list items

--- assets/logic.js
+++ assets/logic.js
@@ -3,3 +3,17 @@ var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
 var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
+
+function renderToDos() {
+
+  listElement.innerHTML = "";
+
+  for (todo of todos) {
+
+    var todoElement = document.createElement("li");
+    var todoText = document.createTextNode(todo);
+    var linkElement = document.createElement("a");
+
+  }
+
+}
