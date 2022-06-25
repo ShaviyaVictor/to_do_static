ec872b821700e8d9b15d4749cc7b8bc7f3f82aee
Victor Shaviya <vs610642@digicelgroup.com>
2022-06-25 13:27:59 -0500

    Create variables for the logic

---
+++ assets/logic.js
@@ -0,0 +1,5 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
